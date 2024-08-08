5393a458ad250cd52136f510ab927f37f4c9d9ea
Merry <merrychrisd29@gmail.com>
2024-08-08 17:21:01 +0800

    Add emailController

--- server/backend/controllers/emailController.js
+++ server/backend/controllers/emailController.js
@@ -1,37 +1,40 @@
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
 
-// Setting up the transporter
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
-
-// Email send function
 const sendEmail = async (req, res) => {
-  const { firstName, lastName, email, phoneNumber, message } = req.body;
-
-  const mailOptions = {
-    from: email,  // Use the email from the form
-    to: 'webapp.sigisdac@gmail.com',  // Target email address
-    subject: 'New Contact Form Submission',
-    text: `You have a new contact form submission from:
-    First Name: ${firstName}
-    Last Name: ${lastName}
-    Email: ${email}
-    Phone Number: ${phoneNumber}
-    Message: ${message}`
+    const { firstName, lastName, email, phoneNumber, message } = req.body;
+  
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: false,  // or true if using SSL on a specific port
+      auth: {
+        user: process.env.SMTP_MAIL,
+        pass: process.env.SMTP_PASS
+      }
+    });
+  
+    const mailOptions = {
+      from: process.env.SMTP_MAIL,
+      to: email,
+      subject: 'New Contact Form Submission',
+      text: `You have a new contact form submission from:
+        First Name: ${firstName}
+        Last Name: ${lastName}
+        Email: ${email}
+        Phone Number: ${phoneNumber}
+        Message: ${message}`
+    };
+  
+    try {
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: 'Email sent successfully' });
+    } catch (error) {
+      console.error('Error sending email:', error);
+      res.status(500).json({ error: 'Failed to send email', details: error.message });
+    }
   };
-
-  try {
-    await transporter.sendMail(mailOptions);
-    res.status(200).send('Email sent successfully');
-  } catch (error) {
-    console.error('Error sending email: ', error);
-    res.status(500).send('Error sending email');
-  }
-};
+  
 
 export { sendEmail };
