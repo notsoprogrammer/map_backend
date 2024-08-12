570aa889ff015f02ffe0de7341986d68d74c7e5b
Merry <merrychrisd29@gmail.com>
2024-08-12 16:42:27 +0800

    set secure to prod

--- server/backend/utils/generateToken.js
+++ server/backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development',
+    secure: process.env.NODE_ENV === 'production',
     sameSite: 'none',
     maxAge: 24 * 60 * 60 * 1000
   });
