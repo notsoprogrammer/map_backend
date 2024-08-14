338580e5ddc1b5e8bd69964b84bc1e2315577a55
Merry <merrychrisd29@gmail.com>
2024-08-14 10:47:39 +0800

    add reset in route

--- server/backend/routes/forgotPWRoutes.js
+++ server/backend/routes/forgotPWRoutes.js
@@ -1,8 +1,9 @@
 import {forgotPassword} from '../controllers/forgotPasswordController.js';
+import { resetPassword } from '../controllers/resetController.js';
 import express from 'express';
 
 const router = express.Router();
 
 router.post('/forgot-password', forgotPassword);
-
+router.post('/reset-password', resetPassword); 
 export default router;
